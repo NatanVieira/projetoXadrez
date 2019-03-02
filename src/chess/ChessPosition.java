07110729184635b79c54bc44bd6d88da14cbf760
natanael.vieira <natanaelvieira130@gmail.com>
2019-03-02 19:40:11 -0300

    Movendo Peças

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -2,11 +2,11 @@ package chess;
 
 import boardgame.Position;
 
-public class ChessPositon {
+public class ChessPosition {
 	private char column;
 	private int  row;
 	
-	public ChessPositon(char column, int row) {
+	public ChessPosition(char column, int row) {
 		if(column < 'a' || column > 'h' || row < 1 || row > 8) {
 			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8");
 		}
@@ -24,8 +24,8 @@ public class ChessPositon {
 	protected Position toPosition() {
 		return new Position(8 - row, column - 'a');
 	}
-	protected static ChessPositon fromPosition(Position position) {
-		return new ChessPositon((char)('a' - position.getColumn()), 8 - position.getRow());
+	protected static ChessPosition fromPosition(Position position) {
+		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
 	}
 	@Override
 	public String toString() {
